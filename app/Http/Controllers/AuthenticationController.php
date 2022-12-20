55f5980f701c717f297a35571b250158bf2721d6
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-12-20 10:03:01 +0800

    Update AuthenticationController.php

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -22,15 +22,15 @@ class AuthenticationController extends Controller
             'password' => 'required',
         ];
 
-        $validator = $request->validate($rules, $messages);
+        $validated_data = $request->validate($rules, $messages);
 
         $data = $request->input();
 
         //SELECT ELOQUENT
-        $check = Authentication::where('username', $req->username)->exists();
+        $check = Authentication::where('username', $request->username)->exists();
 
         if ($check) {
-            $Authentication = Authentication::where('username', $req->username)
+            $Authentication = Authentication::where('username', $request->username)
                 ->get()
                 ->first();
 
@@ -43,17 +43,17 @@ class AuthenticationController extends Controller
                 Session::put('role', $role);
                 Session::put('logged_user', $username); //put the data and in session
 
-                if ($role == 'Dean') {
-                    return redirect('dean-profile');
-                } elseif ($role == 'Students') {
-                    return redirect('students-profile');
-                } elseif ($role == 'Lecturer') {
-                    return redirect('lecturer-profile');
-                } elseif ($role == 'Committee') {
-                    return redirect('committee-profile');
-                } elseif ($role == 'Coordinator') {
-                    return redirect('coordinator-profile');
-                } 
+                // if ($role == 'Dean') {
+                //     return redirect('dean-profile');
+                // } elseif ($role == 'Students') {
+                //     return redirect('students-profile');
+                // } elseif ($role == 'Lecturer') {
+                //     return redirect('lecturer-profile');
+                // } elseif ($role == 'Committee') {
+                //     return redirect('committee-profile');
+                // } elseif ($role == 'Coordinator') {
+                //     return redirect('coordinator-profile');
+                // } 
 
             } else {
                 // custom back validator message
@@ -75,5 +75,49 @@ class AuthenticationController extends Controller
         }
 
         // return $check
+    }
+
+    public function register(Request $request)  {
+        // Validator
+
+        $messages = [
+            'username.required' => 'Username is required',
+            'password.required' => 'Password is required',
+            'role.required' => 'Role is required',
+        ];
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required | email'
+        ];
+
+        $validated_data = $request->validate($rules, $messages);
+
+        $data = $request->input();
+
+        //SELECT ELOQUENT
+        $check = Authentication::where('username', $req->username)->exists();
+
+        if ($check) {
+            // custom back validator message
+            $custom_msg = [
+                'username' => 'Username already exist',
+            ];
+
+            // return back with custom error message
+            return redirect()->back()->withInput()->withErrors($custom_msg);
+        } else {
+            $Authentication = new Authentication;
+            $Authentication->username = $data['username'];
+            $Authentication->password = $data['password'];
+            $Authentication->role = $data['role'];
+            $Authentication->save();
+
+            return redirect('login');
+        }
     }
 }
