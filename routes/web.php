41a1c46832e94a79b04d53d68c35f7d8a00d8c3d
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-12-21 01:09:40 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Router;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
