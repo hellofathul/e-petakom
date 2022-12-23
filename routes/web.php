6afa9ab716169a00d56868baa47e1d45d0f9305d
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-12-23 21:17:52 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use Symfony\Component\Routing\Router;
-use App\Http\Controllers\DeanController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\LecturerController;
 use App\Http\Controllers\CommitteeController;
 use App\Http\Controllers\CoordinatorController;
+use App\Http\Controllers\DeanController;
+use App\Http\Controllers\LecturerController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +16,7 @@ use App\Http\Controllers\CoordinatorController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,7 +25,6 @@ Route::get('/', function () {
 Route::get('/home', function () {
     $logged_user = session()->get('logged_user');
     $role = session()->get('role');
-
 
     if (!$logged_user) {
         return view('layouts.main');
@@ -59,26 +56,24 @@ Route::post('user_reset', 'AuthenticationController@resetpassword');
 Route::get('/logout', 'AuthenticationController@logout');
 
 // STUDENT CONTROLLER
-Route::get  ('student-profile', [StudentController::class, 'index']);
-Route::post  ('student_update', [StudentController::class, 'update']);
+Route::get('student-profile', [StudentController::class, 'index']);
+Route::post('student_update', [StudentController::class, 'update']);
 
 // LECTURER CONTROLLER
-Route::get  ('lecturer-profile', [LecturerController::class, 'index']);
-Route::post  ('lecturer_update', [LecturerController::class, 'update']);
+Route::get('lecturer-profile', [LecturerController::class, 'index']);
+Route::post('lecturer_update', [LecturerController::class, 'update']);
 
 // DEAN CONTROLLER
-Route::get  ('dean-profile', [DeanController::class, 'index']);
-Route::post  ('dean_update', [DeanController::class, 'update']);
+Route::get('dean-profile', [DeanController::class, 'index']);
+Route::post('dean_update', [DeanController::class, 'update']);
 
 // COMMITTEE CONTROLLER
-Route::get  ('committee-profile', [CommitteeController::class, 'index']);
-Route::post  ('committee_update', [CommitteeController::class, 'update']);
+Route::get('committee-profile', [CommitteeController::class, 'index']);
+Route::post('committee_update', [CommitteeController::class, 'update']);
 
 // COORDINATOR CONTROLLER
-Route::get  ('coordinator-profile', [CoordinatorController::class, 'index']);
-Route::post  ('coordinator_update', [CoordinatorController::class, 'update']);
-
-
+Route::get('coordinator-profile', [CoordinatorController::class, 'index']);
+Route::post('coordinator_update', [CoordinatorController::class, 'update']);
 
 Route::redirect('/dhiya', '/');
 
@@ -93,15 +88,15 @@ Route::view('/pelajar', 'pelajar',
 //     echo "<h1>POST --  Kelas Programming</h1>";
 // });
 
-Route::get('/cec', function(){
-    return view('electioncommittee/ce-committee'); 
+Route::get('/cec', function () {
+    return view('electioncommittee/ce-committee');
 });
 
-Route::get('/ces', function(){
+Route::get('/ces', function () {
     return view('electioncommittee/ce-student');
 });
 
-Route::get('/ceo', function(){
+Route::get('/ceo', function () {
     return view('electioncommittee/ce-coordinator');
 });
 
