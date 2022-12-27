b1c6bc89468730baa44bb112ceb81981392cc402
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-12-27 15:57:48 +0800

    Update login-signup.blade.php

--- resources/views/layouts/login-signup.blade.php
+++ resources/views/layouts/login-signup.blade.php
@@ -10,19 +10,21 @@ $roles = session()->get('role');
 <link rel="stylesheet" href="{{ asset('css/login-signup.css') }}">
 
 <div class="cont">
-    <div class="form sign-in">
-        <h2>Welcome back,</h2>
-        <label>
-            <span>Email</span>
-            <input type="email" />
-        </label>
-        <label>
-            <span>Password</span>
-            <input type="password" />
-        </label>
-        <p class="forgot-pass">Forgot password?</p>
-        <button type="button" class="submit">Sign In</button>
-    </div>
+    <form action="user_login" method="post"></form>
+        <div class="form sign-in">
+            <h2>Welcome back,</h2>
+            <label>
+                <span>Email</span>
+                <input type="email" />
+            </label>
+            <label>
+                <span>Password</span>
+                <input type="password" />
+            </label>
+            <p class="forgot-pass">Forgot password?</p>
+            <button type="button" class="submit">Sign In</button>
+        </div>
+    </form>
     <div class="sub-cont">
         <div class="img">
             <div class="img__text m--up">
