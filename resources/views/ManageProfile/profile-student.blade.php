d742d0859a9215ef330e3532e75eff667fbf3745
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-01-05 00:03:46 +0800

    Update profile-student.blade.php

--- resources/views/ManageProfile/profile-student.blade.php
+++ resources/views/ManageProfile/profile-student.blade.php
@@ -79,43 +79,52 @@ $roles = session()->get('role');
             <div class="tertiary__container">
                 <div class="fourth__container">
                     <div class="profile__picture">
-                        <h1>Profile Picture</h1>
+                        <h1>Profile Picture</h1>    
                     </div>
                 </div>
                 <div class="fourth__container">
                     <div class="course__dropdown">
                         <h4>Course</h1>
-                            <select class="form-select course-dropdown" aria-label="Default select example"
-                                name="student_course">
-                                <option selected>Software Engineering</option>
-                                <option value="1">Graphics and Multimedia Technology</option>
-                                <option value="2">Computer System's and Networking</option>
-                                <option value="3">Diploma Computer Science</option>
+                            <select class="form-select course-dropdown" name="student_course">
+                                <option selected>{{ $students[0]->student_course }}</option>
+                                <option value="Software Engineering">Software Engineering</option>
+                                <option value="Graphics and Multimedia Technology">Graphics and Multimedia Technology</option>
+                                <option value="Computer System's and Networking">Computer System's and Networking</option>
+                                <option value="Diploma Computer Science">Diploma Computer Science</option>
                             </select>
+                            {{-- <select name="smsstaff_key" id="smsstaff_key" required>
+                                @foreach ($staff as $staffMember)
+                                    <option value="{{ $staffMember->smsstaff_key }}"
+                                        {{ request()->input('smsstaff_key') == $staffMember->smsstaff_key ? 'selected' : '' }}>
+                                        {{ $staffMember->name }}</option>
+                                @endforeach
+                            </select> --}}
                     </div>
                     <div class="triple__grid">
                         <div class="year__dropdown">
                             <h4>Year</h1>
-                                <select class="form-select" aria-label="Default select example" name="student_year">
-                                    <option selected>1</option>
-                                    <option value="1">2</option>
-                                    <option value="2">3</option>
-                                    <option value="3">4</option>
-                                    <option value="4">5</option>
+                                <select class="form-select" name="student_year">
+                                    <option selected>{{ $students[0]->student_year }}</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
                                 </select>
                         </div>
                         <div class="semester__dropdown">
                             <h4>Semester</h1>
-                                <select class="form-select" aria-label="Default select example" name="student_semester">
-                                    <option selected>1</option>
-                                    <option value="1">2</option>
-                                    <option value="2">3</option>
-                                    <option value="3">4</option>
-                                    <option value="4">5</option>
-                                    <option value="5">6</option>
-                                    <option value="6">7</option>
-                                    <option value="7">8</option>
-                                    <option value="8">9</option>
+                                <select class="form-select" name="student_semester">
+                                    <option selected>{{ $students[0]->student_semester }}</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
                                 </select>
                         </div>
                         <div class="matric__id">
