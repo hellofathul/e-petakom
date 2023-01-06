99eda6d583c0b55048d78a1041011741eee1c6f2
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-01-06 16:40:23 +0800

    Update ce-student-register.blade.php

--- resources/views/electioncommittee/ce-student-register.blade.php
+++ resources/views/electioncommittee/ce-student-register.blade.php
@@ -9,48 +9,48 @@ $roles = session()->get('role');
 @extends('layouts.main')
 
 @section('content')
-            <div class="header">
-                <h1>Committee Election</h1>  
-            </div>
-            <div class="electionregister__container">
-                <br />
-                <h1 style="font-size: 1.8rem;">Register As Candidate</h1>
-                <button class="electionuploadregister">
-                    <img class="file" src="./images/upload-file.png">
-                </button>
-                <br />
-                <a>
-                    <h2 style="text-align:center;">Upload Profile Image</h2>
-                </a>
-                <div class="electionforms__container">
-                    <div class="electionform__container">
-                        <form>
-                            <div class="electionform_inputname_manifesto">
-                                <div class="electionforminputname">
-                                    <h4>Name: </h4>
-                                    <input class="electiontextform_name" type="text" name="studentname" />
-                                </div>
+    <div class="header">
+        <h1>Committee Election</h1>
+    </div>
+    <div class="electionregister__container">
+        <br />
+        <h1 style="font-size: 1.8rem;">Register As Candidate</h1>
+        <button class="electionuploadregister">
+            <img class="file" src="./images/upload-file.png">
+        </button>
+        <br />
+        <a>
+            <h2 style="text-align:center;">Upload Profile Image</h2>
+        </a>
+        <div class="electionforms__container">
+            <div class="electionform__container">
+                <form>
+                    <div class="electionform_inputname_manifesto">
+                        <div class="electionforminputname">
+                            <h4>Name: </h4>
+                            <input class="electiontextform_name" type="text" name="studentname" />
+                        </div>
 
-                                <div class="electionforminputname">
-                                    <h4>Manifesto: </h4>
-                                    <input class="electiontextform_manifesto" type="text" name="studentmanifesto" />
-                                </div>
-                            </div>
-                            <br />
+                        <div class="electionforminputname">
+                            <h4>Manifesto: </h4>
+                            <input class="electiontextform_manifesto" type="text" name="studentmanifesto" />
+                        </div>
+                    </div>
+                    <br />
 
-                            <div class="electionforminputname">
-                                <h4>Matric ID: </h4>
-                                <input class="electiontextform_matricid" type="text" name="studentmatricid" />
-                            </div>
+                    <div class="electionforminputname">
+                        <h4>Matric ID: </h4>
+                        <input class="electiontextform_matricid" type="text" name="studentmatricid" />
+                    </div>
 
-                            <br />
+                    <br />
 
 
-                                <h4>Position to compete for: </h4>
-                                <br />
-                                <h4>Terms & Conditions of becoming a member of the PETAKOM Committee </h4>
-                                <br />
-                                <textarea class="terms" label="terms" rows="7">
+                    <h4>Position to compete for: </h4>
+                    <br />
+                    <h4>Terms & Conditions of becoming a member of the PETAKOM Committee </h4>
+                    <br />
+                    <textarea class="terms" label="terms" rows="7">
 First term
 First term
 First term
@@ -58,20 +58,17 @@ First term
 First term
 First term
                             </textarea>
-                                <br />
-                                <div style="text-align:center">
-                                    <button class="electionnormal"><h3>Cancel</h3></button>
-                                    <button class="electionnormal"><h3>Register</h3></button>
-                                </div>
-
-                        </form>
+                    <br />
+                    <div style="text-align:center">
+                        <button class="electionnormal">
+                            <h3>Cancel</h3>
+                        </button>
+                        <button class="electionnormal">
+                            <h3>Register</h3>
+                        </button>
                     </div>
-                </div>
+
+                </form>
             </div>
-        <!-- END OF MIDDLE -->
-    </main>
-    <!------------- END OF ASIDE------------->
-
-</body>
-
-</html>
+        </div>
+@endsection
